2bf985074c1e72b67c2bd7420522a89defaf71ec
Ardyan Ardasyah <ardyanpersonal@gmail.com>
2026-01-05 21:31:17 +0700

    Add files via upload

--- Apps/Weather.lua
+++ Apps/Weather.lua
@@ -3,6 +3,8 @@
 local city = "Pandaan";
 local data = c3_get_weather(city);
 
+local busy = false
+
 while true do
     c3_cls()
 
@@ -10,20 +12,24 @@ while true do
     c3_print(string.upper(city), 40, 15)
     c3_draw_hline(0, 18, 128)
 
-    if data then
+    if busy then
+        c3_print("Loading...", 35, 40)
+    elseif data then
         c3_print(math.floor(data.temp) .. " Celcius", 30, 35)
         c3_print("Status: " .. data.desc, 10, 50)
-        c3_print("Humidity: " .. data.hum .. "%", 10, 60)
+        c3_print("Humidity " .. data.hum .. "%", 10, 60)
     else
-        c3_print("Failed to load data!", 15, 40)
+        c3_print("Press OK to load!", 25, 40)
     end
 
     c3_update()
 
-    local btn = c3_btn("ok")
-    if btn == 1 then
+    local ev = c3_btn("ok")
+    if ev == 1 and not busy then
+        busy = true
         data = c3_get_weather(city)
-    elseif btn == 2 then
+        busy = false
+    elseif ev == 2 then
         break
     end
 
